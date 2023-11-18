5d17b4e5cfa27f02204674057774d42863e18fbf
Anton Zavalniuk <antonzavalniuk@gmail.com>
2023-11-19 00:12:16 +0100

    add button to create new todo and creation form. Install dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Columns from '@/components/columns'
+import NewTodoDialog from '@/components/new-todo-dialog'
 
 export default function Home() {
   return (
     <main className='flex h-screen bg-gradient-to-br from-gray-700 to-gray-900 py-12 text-white'>
       <div className='mx-auto w-full max-w-7xl px-6'>
+        <NewTodoDialog />
         <Columns />
       </div>
     </main>
--- components/columns.tsx
+++ components/columns.tsx
@@ -2,7 +2,7 @@ import Column from './column'
 
 export default function Columns() {
   return (
-    <section className='flex h-full gap-6 lg:gap-12'>
+    <section className='mt-12 flex h-full gap-6 lg:gap-12'>
       <Column title='Todo' status='TODO' />
       <Column title='In Progress' status='IN_PROGRESS' />
       <Column title='Done' status='DONE' />
